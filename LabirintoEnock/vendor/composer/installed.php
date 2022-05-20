95c9a1b74badab07c0a4ce74413fd5d85df5f0b4
Paula Vieira <paulabeatrizrv@gmail.com>
2022-05-20 09:58:10 -0300

    Add files via upload

---
+++ LabirintoEnock/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'krakjoe/pthreads-polyfill' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../krakjoe/pthreads-polyfill',
+            'aliases' => array(),
+            'reference' => 'e0b6b34aca142dda943bf8ade046285fd2000c35',
+            'dev_requirement' => false,
+        ),
+    ),
+);
